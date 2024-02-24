c4c3f1b131e544a6ff3b98099d84c0fa7dc43a3b
alrocar <alrocar@tinybird.co>
2024-02-24 18:37:14 +0100

    add app

--- aa.sh
+++ aa.sh
@@ -6,20 +6,21 @@ while true; do
   current_date=$(date -u +'%Y-%m-%d %H:%M:%S')
 
   locked_screen=$(ddcctl -d 1 | grep "Failed to poll")
+  active_app=$(osascript -e 'tell application "System Events" to get name of application processes whose frontmost is true')
 
   if [ -n "$locked_screen" ]; then
     echo "Locked screen!"
     curl \
       -X POST 'https://api.tinybird.co/v0/events?name=events&wait=false' \
       -H "Authorization: Bearer $LOCKED_TB_TOKEN" \
-      -d "{\"timestamp\":\"$current_date\",\"status\":\"locked\",\"user\":\"$LOCKED_SCREEN_USER\",\"duration\":$LOCKED_SCREEN_SLEEP_TIME}" \
+      -d "{\"timestamp\":\"$current_date\",\"status\":\"locked\",\"user\":\"$LOCKED_SCREEN_USER\",\"duration\":$LOCKED_SCREEN_SLEEP_TIME,\"app\":\"$active_app\"}" \
       &
   else
     echo "Okay Let's Go!"
     curl \
       -X POST 'https://api.tinybird.co/v0/events?name=events&wait=false' \
       -H "Authorization: Bearer $LOCKED_TB_TOKEN" \
-      -d "{\"timestamp\":\"$current_date\",\"status\":\"unlocked\",\"user\":\"$LOCKED_SCREEN_USER\",\"duration\":$LOCKED_SCREEN_SLEEP_TIME}" \
+      -d "{\"timestamp\":\"$current_date\",\"status\":\"unlocked\",\"user\":\"$LOCKED_SCREEN_USER\",\"duration\":$LOCKED_SCREEN_SLEEP_TIME,\"app\":\"$active_app\"}" \
       &
   fi
 
