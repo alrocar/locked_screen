787a291f75ef76bf83547ab9ac3dc04c1569ae8b
alrocar <alrocar@tinybird.co>
2024-02-27 20:24:05 +0100

    report active tab domain

--- aa.sh
+++ aa.sh
@@ -7,20 +7,22 @@ while true; do
 
   locked_screen=$(ddcctl -d 1 | grep "Failed to poll")
   active_app=$(osascript -e 'tell application "System Events" to get name of application processes whose frontmost is true')
+  active_tab_url=$(osascript -e 'tell application "Arc" to get URL of active tab of window 1')
+  active_domain=$(echo "$url" | awk -F/ '{print $3}')
 
   if [ -n "$locked_screen" ]; then
     echo "Locked screen!"
     curl \
       -X POST 'https://api.tinybird.co/v0/events?name=events&wait=false' \
       -H "Authorization: Bearer $LOCKED_TB_TOKEN" \
-      -d "{\"timestamp\":\"$current_date\",\"status\":\"locked\",\"user\":\"$LOCKED_SCREEN_USER\",\"duration\":$LOCKED_SCREEN_SLEEP_TIME,\"app\":\"$active_app\"}" \
+      -d "{\"timestamp\":\"$current_date\",\"status\":\"locked\",\"user\":\"$LOCKED_SCREEN_USER\",\"duration\":$LOCKED_SCREEN_SLEEP_TIME,\"app\":\"$active_app\",\"domains\":[\"$active_domain\"]}" \
       &
   else
     echo "Okay Let's Go!"
     curl \
       -X POST 'https://api.tinybird.co/v0/events?name=events&wait=false' \
       -H "Authorization: Bearer $LOCKED_TB_TOKEN" \
-      -d "{\"timestamp\":\"$current_date\",\"status\":\"unlocked\",\"user\":\"$LOCKED_SCREEN_USER\",\"duration\":$LOCKED_SCREEN_SLEEP_TIME,\"app\":\"$active_app\"}" \
+      -d "{\"timestamp\":\"$current_date\",\"status\":\"unlocked\",\"user\":\"$LOCKED_SCREEN_USER\",\"duration\":$LOCKED_SCREEN_SLEEP_TIME,\"app\":\"$active_app\",\"domains\":[\"$active_domain\"]}" \
       &
   fi
 
